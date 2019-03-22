653d682cf156428e909718c4e802c55441121019
Edvard Pires Bjørgen <edbjorgen@gmail.com>
2019-03-22 15:29:24 +0100

    started on register wins and losses

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const baseUrl = "localhost:8000";
 
 app.put('/switch/user', (req, res) => {
   const {userName} = req.body;
-  const userInfo = { userName: userName, creationDate : new Date().getTime()};
+  const userInfo = { userName: userName, creationDate : new Date().getTime(), score: score};
 
   db.ref(`user/${userName}`).setLocale({userInfo}, error => {
     if (error) {
@@ -37,6 +37,7 @@ app.put('/switch/user', (req, res) => {
   });
 });
 
+/**POST**/
 app.post('/switch/user', async (req, res) => {
   const {userName} = req.body;
 
@@ -45,20 +46,26 @@ app.post('/switch/user', async (req, res) => {
     if (!snapshot.exists()) {
       db.ref(`user/`).push({
         userName: userName,
-        creationDate : new Date().getTime()}, error => {
-          if (error) {
-            res.sendStatus(500);
-          } else {
-            res.sendStatus(201);
-          }
-        });
-      } else {
-        console.log("User exists");
-        res.send("User exists");
-      }
-    });
+        creationDate : new Date().getTime(),
+        score: 1500,
+        wins: 0,
+        lose: 0
+      },
+      error => {
+        if (error) {
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    } else {
+      console.log("User exists");
+      res.send("User exists");
+    }
+  });
 });
 
+/**GET**/
 app.get('/switch/user/', (req, res) => {
   const {userName} = req.query;
 
@@ -67,11 +74,19 @@ app.get('/switch/user/', (req, res) => {
     if (snapshot.exists()) {
       const response = Object.assign({}, snapshot.val());
       res.send(response);
-      } else {
-        res.send("user doesnt exist");
+    } else {
+      res.send("user doesnt exist");
 
-      }
-    });
+    }
+  });
+});
+
+app.get('/switch/users/', async (req, res) => {
+  const {gameName} = req.query;
+  const userSnapshot = await db.ref(`user/`).once('value');
+
+  const response = Object.assign({}, userSnapshot.val());
+  res.send(response);
 });
 
 
@@ -85,7 +100,8 @@ app.post('/switch/game', async (req, res) => {
     if (!snapshot.exists()) {
       db.ref(`game/`).push({
         gameName: gameName,
-        creationDate : new Date().getTime()}, error => {
+        creationDate : new Date().getTime()},
+        error => {
           if (error) {
             res.sendStatus(500);
           } else {
@@ -97,33 +113,80 @@ app.post('/switch/game', async (req, res) => {
         res.send("Game exists");
       }
     });
-});
+  });
 
-app.get('/switch/game/', (req, res) => {
-  const {gameName} = req.query;
+  app.get('/switch/game/', (req, res) => {
+    const {gameName} = req.query;
 
-  var ref = db.ref('game').orderByChild('gameName').equalTo(gameName);
-  ref.once('value', snapshot => {
-    if (snapshot.exists()) {
-      const response = Object.assign({}, snapshot.val());
-      res.send(response);
+    var ref = db.ref('game').orderByChild('gameName').equalTo(gameName);
+    ref.once('value', snapshot => {
+      if (snapshot.exists()) {
+        const response = Object.assign({}, snapshot.val());
+        res.send(response);
       } else {
         res.send("Game doesnt exist");
 
       }
     });
-});
+  });
 
-app.get('/switch/games/', async (req, res) => {
-  const {gameName} = req.query;
-  const gameSnapshot = await db.ref(`game/`).once('value');
+  app.get('/switch/games/', async (req, res) => {
+    const {gameName} = req.query;
+    const gameSnapshot = await db.ref(`game/`).once('value');
 
-  const response = Object.assign({}, gameSnapshot.val());
-  res.send(response);
-});
+    const response = Object.assign({}, gameSnapshot.val());
+    res.send(response);
+  });
+
+
+  /************************************** SCORE **********************************************/
+  app.post('/switch/register', (req, res) => {
+    const {winner, losers} = req.body;
+    registerWinnerToFireBase(winner);
+  });
+  function registerWinnerToFireBase(userWinner){
+
+    const userKey = Object.keys(userWinner)[0];
+
+
+    var ref = db.ref('user').orderByKey().equalTo(userKey);
+    ref.once('value', snapshot => {
+      //console.log(snapshot.child(userKey));
+      let userName = "";
+      const newWinCount = 0;
+      const newLoseCount = 0;
+      const newScore = 0;
+      snapshot.forEach(child => {
+        const user = child.val();
+
+        const newWinCount = user.wins + 1;
+        const newLoseCount = user.lose+ 1;
+        const newScore = calculateWinScore( user.score);
+
+        snapshot.ref.update({
+          wins: newWinCount,
+          lose: newLoseCount,
+          score: newScore,
+        });
+      });
+
+    },
+    error => {
+      if (error) {
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(201);
+      }
+    });
+  }
 
 
 
-var port = process.env.PORT || 8000;
-app.listen(port, "0.0.0.0");
-console.log('Running on http://localhost:8000');
+
+  function calculateWinScore(score) {
+    return score + 10;
+  }
+
+  var port = process.env.PORT || 8000;
+  app.listen(port, "0.0.0.0");
+  console.log('Running on http://localhost:8000');
